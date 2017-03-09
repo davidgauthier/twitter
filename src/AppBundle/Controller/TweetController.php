5f12cd4ad776f81f52d74891a6d3f15e450cb90d
David GAUTHIER <davidgauthier75@gmail.com>
2017-03-09 14:27:03 +0100

    action dans controller, form et vue pour l'ajout de tweet + maj layout

--- src/AppBundle/Controller/TweetController.php
+++ src/AppBundle/Controller/TweetController.php
@@ -7,6 +7,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Form\TweetType;
+
 class TweetController extends Controller
 {
     /**
@@ -26,6 +28,43 @@ class TweetController extends Controller
                 'listTweets' => $listTweets,
             ]
         );
+    }
+
+    /**
+     * @Route("/tweet/new/", name="app_tweet_new", methods={"GET", "POST"} )
+     */
+    public function newAction(Request $request)
+    {
+        // On crée un objet Tweet
+        $tweet = new Tweet();
+        // On récup le form
+        $formTweet = $this->createForm(TweetType::class, $tweet);
+
+        // On fait le lien Requête <-> Formulaire
+        // À partir de maintenant, la variable $tweet contient les valeurs entrées dans le formulaire
+        $formTweet->handleRequest($request);
+
+        // On vérifie que les valeurs entrées sont correctes
+        if ($formTweet->isSubmitted() && $formTweet->isValid()) {
+            // On enregistre notre objet $tweet dans la base de données
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($tweet);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', 'Tweet bien enregistrée.');
+
+            // On redirige vers la page de visualisation du tweet nouvellement créé
+            return $this->redirectToRoute('app_tweet_view', array(
+                'id' => $tweet->getId(),
+            ));
+        }
+
+        // À ce stade, le formulaire n'est pas valide car :
+        // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+        // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
+        return $this->render(':tweet:new.html.twig', array(
+            'formTweet' => $formTweet->createView(),
+        ));
     }
 
 
@@ -49,6 +88,10 @@ class TweetController extends Controller
             ]
         );
     }
+
+
+
+
 
 
 }
